1e96fa1b21e444ac576284c34d4a3b48c5ddc832
Sergio Arias <s-andres140@hotmail.com>
2024-03-26 11:45:30 -0500

    actualizacion

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,18 +1,25 @@
 
 export const MovieList = ({movies,favouriteComponent,handleFavouritesClick}) => {
   const  FavouriteComponent =favouriteComponent
+  
+  if (!movies || movies.length === 0) {
+    return <div>Aun no has buscado peliculas</div>;
+  }
+
   return (
     <>
-    {
-        movies.map(movie =>(
-            < div key={movie.imdbID} className='image-container w-[220px] inline-block m-2 cursor-pointer hover:scale-105 ease-in-out duration-300'>
-                <img  src={movie.Poster} alt='movie'></img>
-                <div onClick={()=>handleFavouritesClick(movie)} className="overlay" >
-                    <FavouriteComponent/>
-                </div>
-              </div>
-            
-            ))
+    { 
+      movies.map(movie =>(
+        < div key={movie.imdbID} className='image-container w-[220px] inline-block m-2 cursor-pointer hover:scale-105 ease-in-out duration-300'>
+            <img  src={movie.Poster} alt='movie'></img>
+            <div onClick={()=>handleFavouritesClick(movie)} className="overlay" >
+                <FavouriteComponent/>
+            </div>
+          </div>
+        
+        ))
+    
+        
     }
     </>
   )
