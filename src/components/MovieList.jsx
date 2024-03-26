6a827f8796044c6ade941ff2f8fed49888fb1fcd
Sergio Arias <s-andres140@hotmail.com>
2024-03-26 09:09:52 -0500

    cambios en el image-container que hacia que el overlay no funcionara bien

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,12 +5,12 @@ export const MovieList = ({movies}) => {
     <>
     {
         movies.map(movie =>(
-            < div key={movie.imdbID} className='image-container w-[220px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300'>
+            < div key={movie.imdbID} className='image-container w-[220px] inline-block m-2 cursor-pointer hover:scale-105 ease-in-out duration-300'>
                 <img  src={movie.Poster} alt='movie'></img>
                 <div className="overlay" >
                     <AddFavourites/>
                 </div>
-            </div>
+              </div>
             
             ))
     }
