28bbb4172b04833c4d6b188a799f87d79f09dce4
Sergio Arias <s-andres140@hotmail.com>
2024-03-31 17:51:20 -0500

    actualizacion de estilos barra de busqueda

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,8 +1,8 @@
 export const SearchBox = ({searchValue, setSearchValue}) => {
   return (
     <div className="flex items-center justify-center py-5">
-        <input 
-            className="w-full max-w-md px-2 py-1 border-2 border-gray-300 rounded"
+    <input 
+        className="w-full max-w-md px-4 py-2 leading-tight text-black transition duration-150 ease-in-out border-2 border-gray-300 rounded-lg focus:outline-none focus:bg-white focus:border-indigo-500"
             type="text" 
             placeholder="Escribe para buscar..."
             value={searchValue}
